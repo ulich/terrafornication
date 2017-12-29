a556e4a81e77e2b7c543f6bb503629a2cedd085f
Ulich, Timo <timo@ulich.net>
2017-12-29 23:26:58 +0100

    Support easy referencing of resources
    fixes #1

--- terrafornication/provider.py
+++ terrafornication/provider.py
@@ -13,14 +13,25 @@ class Provider:
             raise DuplicateResourceException("The resource {}.{} already exists".format(full_type, name))
 
         if "alias" in self.properties:
-            new_props = properties.copy()
-            new_props["provider"] = self.type + "." + self.properties["alias"]
-            self.resources[full_type][name] = new_props
-        else:
-            self.resources[full_type][name] = properties
+            properties = properties.copy()
+            properties["provider"] = self.type + "." + self.properties["alias"]
+
+        self.resources[full_type][name] = properties
+        return Resource(full_type, name, properties)
 
     def resource_dict(self):
         return self.resources
+
+
+class Resource:
+    
+    def __init__(self, type, name, properties):
+        self.type = type
+        self.name = name
+        self.properties = properties
+
+    def ref(self, property):
+        return "${{{}.{}.{}}}".format(self.type, self.name, property)
 
 
 class DuplicateResourceException(RuntimeError):
--- terrafornication/tests/test_terrafornication.py
+++ terrafornication/tests/test_terrafornication.py
@@ -33,9 +33,34 @@ class TestTerrafornication(TestCase):
             }
         })
 
+
     def test_duplicate_resources(self):
         aws = self.tf.provider("aws", {})
         aws.resource('instance', 'app1', {"foo": "bar"})
 
         with self.assertRaises(DuplicateResourceException):
             aws.resource('instance', 'app1', {"foo": "baz"})
+
+
+    def test_resource_references(self):
+        aws = self.tf.provider("aws", {})
+        instance = aws.resource('instance', 'app1', {})
+        aws.resource('route53_record', instance.name + '_dns', {
+            "records": [instance.ref('public_ip')]
+        })
+
+        self.assertEqual(self.tf.to_dict(), {
+            "provider": [{
+                "aws": {}
+            }],
+            "resource": {
+                "aws_instance": {
+                    "app1": {},
+                },
+                "aws_route53_record": {
+                    "app1_dns": {
+                        "records": ["${aws_instance.app1.public_ip}"]
+                    }
+                }
+            }
+        })
