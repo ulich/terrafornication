870e937f91c1482e4cb5b5af1cfa9cd3eecf3877
Ulich, Timo <timo@ulich.net>
2018-01-02 22:45:02 +0100

    add setup.py
    so this package can already be installed via `pip install git+https://github.com/ulich/terrafornication.git`

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='terrafornication',
+      version='0.1.0',
+      description='Describe your infrastructure with terraform as python code',
+      url='http://github.com/ulich/terrafornication',
+      author='Timo Ulich',
+      license='MIT',
+      packages=['terrafornication'],
+      zip_safe=False)
